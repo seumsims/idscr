6374d9e6a0dc63da0089d1b8d3bef05f5726feb9
seum <naasyakoot@outlook.com>
2019-03-10 21:20:34 +0600

    takes some codes

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
 <?php
 
+class Some {
+
+
+public some = "salam";
+
+}
+
+$task = new Some();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php var_dump( $task -> some );  ?></h1>
+</body>
+</html>
+
+
+
+
