9251758f01cf963ccb2713c36651ab94a2cb9664
seum <naasyakoot@outlook.com>
2019-03-11 10:23:18 +0600

    new error detected title showing directly @ screen

---
+++ test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <!--    <?php include( "klus.php" ); ?>-->
+</head>
+<body>
+
+</body>
+</html>
